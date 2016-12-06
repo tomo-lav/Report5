287ea79fb352517146177754b7cdd36fdf4ae6e2
Tomoya Miyara <e165709@ie.u-ryukyu.ac.jp>
2016-12-06 16:54:04 +0900

    step2_ver.1

--- src/main/jp/ac/uryukyu/ie/e165709/Main.java
+++ src/main/jp/ac/uryukyu/ie/e165709/Main.java
@@ -1,9 +1,15 @@
 package jp.ac.uryukyu.ie.e165709;
 
 public class Main {
-    public static void main (String[] args){
 
+    public static void main (String[] args){
+        try {
         String str = null ;
         System.out.println(str.length());
+        }catch(NullPointerException e) {
+            System.out.println("Warning: Exception detected");
+            e.printStackTrace();
+
+        }
     }
 }
