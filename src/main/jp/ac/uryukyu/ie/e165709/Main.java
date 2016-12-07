8e560b3f425fef94ebb880c702a3f0e3f2338f69
Tomoya Miyara <e165709@ie.u-ryukyu.ac.jp>
2016-12-07 18:37:25 +0900

    step3_ver.1

--- src/main/jp/ac/uryukyu/ie/e165709/Main.java
+++ src/main/jp/ac/uryukyu/ie/e165709/Main.java
@@ -3,13 +3,14 @@ package jp.ac.uryukyu.ie.e165709;
 public class Main {
 
     public static void main (String[] args){
+        String str = "3．14" ;
         try {
-        String str = null ;
         System.out.println(str.length());
         }catch(NullPointerException e) {
             System.out.println("Warning: Exception detected");
             e.printStackTrace();
 
         }
+        double value = Double.parseDouble(str);
     }
 }
